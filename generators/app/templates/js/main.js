68e53235f0a894586287f6b31e64a43cf459d11d
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-08-31 10:33:50 +0200

    :art: script organization + add requestAnimFrame

--- generators/app/templates/js/main.js
+++ generators/app/templates/js/main.js
@@ -1,16 +1,29 @@
-/**** VARIABLES ****/
+'use strict';
 
-
-/**** INIT ****/
 $(function(){
 
-    $(document).scroll(function(){
+    var body = $('body');
+    var windowWidth = $(window).width(), windowHeight = $(window).height();
+
+    window.requestAnimFrame = require('./requestAnimFrame.js');
+
+
+
+    // isMobile.any ? body.addClass('is-mobile') : body.addClass('is-desktop');
+
+
+    $(window).on('resize', function(){
+
+        windowWidth = $(window).width();
+        windowHeight = $(window).height();
+
+	}).on('load', function(){
+
+	});
+
+
+    $(document).on('scroll', function(){
+
     });
-
-    $(window).resize(function(){
-	});
-
-	$(window).load(function(){
-	});
 
 });
---
+++ generators/app/templates/js/requestAnimFrame.js
@@ -0,0 +1,8 @@
+module.exports = function(){
+    return window.requestAnimationFrame       ||
+           window.webkitRequestAnimationFrame ||
+           window.mozRequestAnimationFrame    ||
+           window.oRequestAnimationFrame      ||
+           window.msRequestAnimationFrame     ||
+           function(callback){ window.setTimeout(callback, 1000/60); };
+};
