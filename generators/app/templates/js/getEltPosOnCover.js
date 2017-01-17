2a241ede04fc132647e5691c345e41886a5c0740
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-17 14:26:35 +0100

    :art: add getEltPosOnCover script

---
+++ generators/app/templates/js/getEltPosOnCover.js
@@ -0,0 +1,28 @@
+var $ = require('./libs/jquery/dist/jquery.slim.min.js');
+
+module.exports = function(container, imgRatio, imgW, imgH, elt){
+    containerH = container.outerHeight();
+    containerW = container.width();
+    containerRatio = containerH / containerW;
+
+    posX = elt.data('x');
+    posY = elt.data('y');
+
+    if(containerRatio > imgRatio){
+        // portrait
+        finalH = containerH;
+        finalW = imgW*finalH / imgH;
+        newX = finalW*posX / imgW - (finalW - containerW)/2;
+        newY = finalH*posY / imgH;
+    }else{
+        // paysage
+        finalW = containerW;
+        finalH = imgH*finalW / imgW;
+        newX = finalW*posX / imgW;
+        newY = finalH*posY / imgH - (finalH - containerH)/2;
+    }
+
+    ratioScale = (finalH / imgH).toFixed(3);
+
+    return [newX, newY, ratioScale];
+}
