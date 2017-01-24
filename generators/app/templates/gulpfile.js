5a0f765b665c471fe11780b302fcd63aa148bd26
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-01-24 18:00:17 +0100

    :bug: remove duplicate require

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -4,7 +4,6 @@ var path = require('path');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
