4c4b88853312e0fdbe180cb6eb8ae2fe6a65ea9e
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-09-05 23:20:41 +0200

    :art: update browsers gulpfile

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -30,13 +30,13 @@ gulp.task('styles', function () {
         .on('error', reportError)
         .pipe($.autoprefixer({
             browsers: [
-            'ie >= 10',
-            'ie_mob >= 10',
-            'ff >= 30',
-            'chrome >= 34',
-            'safari >= 7',
+            'ie >= 11',
+            'ie_mob >= 11',
+            'ff >= 40',
+            'chrome >= 50',
+            'safari >= 9',
             'opera >= 23',
-            'ios >= 7',
+            'ios >= 9',
             'android >= 4.4',
             'bb >= 10'
             ]
