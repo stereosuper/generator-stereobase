2b2fc86f211f1ac869ede9170f7502db35451f47
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-11-27 16:09:21 +0100

    :art: wp config localhost

--- generators/app/templates/wp-config.php
+++ generators/app/templates/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', '<%= dbuser %>');
 define('DB_PASSWORD', '<%= dbpass %>');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
