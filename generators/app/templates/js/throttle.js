0efbc485d7c93145806f9794a8e38c3a7fb21767
Elisabeth Hamel <elisabeth@stereosuper.fr>
2018-03-01 11:32:09 +0100

    :bug: fix undefined variable

--- generators/app/templates/js/throttle.js
+++ generators/app/templates/js/throttle.js
@@ -1,15 +1,17 @@
 module.exports = function( callback, delay ){
+
     let last, timer;
 
-    function reset(){
-        last = now;
-        callback.apply( context, args );
-    }
     
     return function(){
         const context = this, now = +new Date(), args = arguments;
 
-        
+
+        function reset(){
+            last = now;
+            callback.apply( context, args );
+        }
+
         
         if( last && now < last + delay ){
             // le délai n'est pas écoulé on reset le timer
@@ -20,4 +22,5 @@ module.exports = function( callback, delay ){
             reset();
         }
     };
+    
 }
